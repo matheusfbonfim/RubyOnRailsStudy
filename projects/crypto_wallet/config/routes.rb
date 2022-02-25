bb3ef8a6639a57a85537781d63b8eff24b7d937a
matheusfbonfim <matheusbonfim@alunos.utfpr.edu.br>
2022-02-25 14:36:54 -0300

    Trabalhando com Models via Rails Console

--- projects/crypto_wallet/config/routes.rb
+++ projects/crypto_wallet/config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   # Indica o metodo GET -> Primeiro o controller e depois a action
-  get('welcome/index')
+  get 'welcome/index'# , to: 'welcome#index' # -> Infere que atraves do path quem é o parametro e a action
+  # get('/inicio', to: 'welcome#index')
 
   # Recursos 
-  resources :coins
+  resources :coins # -> Gera 7 rotas de forma automática (GET, POST, ....)
+  # get('/coins', to:'coins#index')
 
   # Página padrão vai para welcome 
   root to: 'welcome#index'
 
-  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
