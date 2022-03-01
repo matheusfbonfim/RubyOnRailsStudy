2cf01c49af79d3504a2b4630b3913a5b61f2fbaa
Matheus Bonfim <matheusbonfim@alunos.utfpr.edu.br>
2022-02-28 23:23:59 -0300

    Belongs to

--- projects/crypto_wallet/app/models/coin.rb
+++ projects/crypto_wallet/app/models/coin.rb
@@ -1,2 +1,7 @@
 class Coin < ApplicationRecord
+  # Pertence a 
+  # A tabela de Coin vai conversar com outra tabela -> Mining Type
+  # Id de uma tabela estrangeira na tabela de coin -> Pertence a mining type
+  # Existe um id que relaciona com outra tabela
+  belongs_to :mining_type
 end
