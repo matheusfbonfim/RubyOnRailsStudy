a913c221ab3c7c986589e646295a41e0216bafb5
Matheus Bonfim <matheusbonfim@alunos.utfpr.edu.br>
2022-03-03 00:28:57 -0300

    Primeiro passos com o Bootstrap

--- projects/crypto_wallet_5_2/config/initializers/assets.rb
+++ projects/crypto_wallet_5_2/config/initializers/assets.rb
@@ -12,14 +12,12 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 Rails.application.config.assets.precompile += %w( coins.css
-                                                  mining_type.css
+                                                  mining_types.css
                                                   scaffolds.css
                                                   welcome.css)
 
 Rails.application.config.assets.precompile += %w( cable.js
                                                   coins.js 
-                                                  mining_type.js
+                                                  mining_types.js
                                                   scaffolds.js
-                                                  welcome.js
-                                                  jquery.js
-                                                  notify.js)                                                 
+                                                  welcome.js)                                                 
