3261b7d3e09592965ddd918eccca81bffebd0e8f
matheusfbonfim <matheusbonfim@alunos.utfpr.edu.br>
2022-03-19 10:30:27 -0300

    deleted all file - crpyto_wallet - old version

--- projects/crypto_wallet/db/migrate/20220301010057_add_mining_type_to_coins.rb
+++
@@ -1,9 +0,0 @@
-class AddMiningTypeToCoins < ActiveRecord::Migration[7.0]
-  def change
-    # Adiciona uma referencia na tabela coin - Nome da tabela que vai ficar o campo
-    # Campo mining type -> Nome do campo a ser adicionado
-    # Chave estrangeira -> Por convenção o Rails vai procurar um nome de tabela "Mining Types" e faz a referência
-    # Tudo isso devido a padronização do nome
-    add_reference :coins, :mining_type, foreign_key: true
-  end
-end
