4e1720c1a5705cd336454f538806d48aed78e3f0
MatheusBonfim <matheusbonfim@alunos.utfpr.edu.br>
2023-05-07 22:43:26 -0300

    Remove Old Files

--- projects/crypto_wallet_5_2/config/initializers/locale.rb
+++
@@ -1,7 +0,0 @@
-# Permitted locales available for the application
-# Indica quais são os idiomas disponíveis
-I18n.available_locales = [:en, "pt-BR"]
-
-# Set default locale to something other than :en
-# Idioma padrão
-I18n.default_locale = "pt-BR"
--- projects/simple_market_list/app/models/market_list.rb
+++
@@ -1,2 +0,0 @@
-class MarketList < ApplicationRecord
-end
