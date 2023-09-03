4f6e5cb5e0c5d93ca127880c498323a74387ceb2
Matheus Bonfim <matheusbonfim@alunos.utfpr.edu.br>
2023-09-03 13:14:22 -0300

    feat: Criando o formulário de Edição para o Admin

--- projects/time_to_answer/app/controllers/admins_backoffice/admins_controller.rb
+++ projects/time_to_answer/app/controllers/admins_backoffice/admins_controller.rb
@@ -1,13 +1,17 @@
 class AdminsBackoffice::AdminsController < AdminsBackofficeController
+  before_action :set_resource, only: [:edit]
+  
   def index
     @admins = Admin.all
   end
 
   def edit
-
   end
 
   def destroy
+  end
 
+  def set_resource
+    @admin = Admin.find(params[:id])
   end
 end
