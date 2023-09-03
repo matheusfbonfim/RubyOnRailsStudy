50a1afdf68bf5bc2ef525f635f6f7c539e3f0327
Matheus Bonfim <matheusbonfim@alunos.utfpr.edu.br>
2023-09-03 13:49:21 -0300

    feat: Finalizano o formulário de edição do Admin

--- projects/time_to_answer/app/controllers/admins_backoffice/admins_controller.rb
+++ projects/time_to_answer/app/controllers/admins_backoffice/admins_controller.rb
@@ -1,5 +1,5 @@
 class AdminsBackoffice::AdminsController < AdminsBackofficeController
-  before_action :set_resource, only: [:edit]
+  before_action :set_resource, only: [:edit, :update]
   
   def index
     @admins = Admin.all
@@ -8,10 +8,19 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
   def edit
   end
 
-  def destroy
+  def update
+    if @admin.update(safe_parameters)
+      redirect_to admins_backoffice_admins_path, notice: "Administrador atualizado com sucesso!"
+    else
+      render :edit
+    end
   end
 
   def set_resource
     @admin = Admin.find(params[:id])
+  end
+
+  def safe_parameters
+    params.require(:admin).permit(:email, :password, :password_confirmation)
   end
 end
