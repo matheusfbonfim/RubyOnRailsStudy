10adfac48384201d2d9aee30c52be6347dc02abd
Matheus Bonfim <matheusbonfim@alunos.utfpr.edu.br>
2023-09-13 00:30:13 -0300

    feat: Listando as perguntas e respostas na index

--- projects/time_to_answer/app/models/question.rb
+++ projects/time_to_answer/app/models/question.rb
@@ -3,4 +3,7 @@ class Question < ApplicationRecord
   
   has_many :answers
   accepts_nested_attributes_for :answers, reject_if: :all_blank, allow_destroy: true
+
+  # Kaminari
+  paginates_per 5
 end
